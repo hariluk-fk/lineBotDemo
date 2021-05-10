669e1717446d4cc6f54ef2dcb7b24e36090b1dde
hariluk <hariluk00@gmail.com>
2021-05-10 20:11:50 +0700

    22222

--- app.js
+++ app.js
@@ -16,9 +16,35 @@ const app = express();
 
 var requestTxt = '';
 
+const LINE_MESSAGING_API = "https://api.line.me/v2/bot/message";
+const LINE_HEADER = {
+  "Content-Type": "application/json",
+  "Authorization": "Bearer " + config.CHANNEL_ACCESS_TOKEN
+};
+
+// exports.BasicMessage = functions.https.onRequest((req, res) => {
+//   return 
+// });
+
 setInterval(() => {
-    client.pushMessage('tongpluss', 'ทดสอบ', false);
+    // client.pushMessage('tongpluss', 'ทดสอบ', false);
     // client.replyMessage('d315ebb9e9794cffbcb465249cfa32d5', 'ทดสอบจ้า')
+    request({
+        method: "POST",
+        uri: `${LINE_MESSAGING_API}/push`,
+        headers: LINE_HEADER,
+        body: JSON.stringify({
+          to: "tongpluss",
+          messages: [{
+              type: "text",
+              text: "LINE \uDBC0\uDC84 x \uDBC0\uDCA4 Firebase"
+          }]
+        })
+      }).then(() => {
+          return res.status(200).send("Done");
+      }).catch(error => {
+          return Promise.reject(error);
+      });
 }, 5000);
 
 app.get('', (req, res)=> {
